ac44ec6a67f5c528a8a3836ac87119418e5cacda
ra2457001 <ra2457001@LDI309>
2025-04-24 08:25:51 -0300

    aaaaaaaaaaaaaaaaaaaaaaa

--- ListaTelefonica/Form1.cs
+++ ListaTelefonica/Form1.cs
@@ -12,18 +12,50 @@ namespace ListaTelefonica
 {
     public partial class Form1 : Form
     {
-        string[,] lista;
+        string[][] lista;
         readonly int MAX = 100;
-        int itens = 0;
         public Form1()
         {
             InitializeComponent();
-            lista = new string[MAX, 2];
+            lista = new string[MAX][];
 
         }
 
-        
+        int Length(string[] s)
+        {
+            int itens = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != null)
+                    itens++;
+            }
+            return itens;
+        }
 
+        int Length(string[][] s)
+        {
+            int itens = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != null)
+                    itens++;
+            }
+            return itens;
+        }
+        void Atualizar()
+        {
+            dgvLista.Rows.Clear();
+            for (int i = 0; i < Length(lista); i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dgvLista);
+                for (int j = 0; j < Length(lista[i]); j++)
+                {
+                    row.Cells[j].Value = lista[i][j];
+                }
+                dgvLista.Rows.Add(row);
+            }
+        }
         private void btAdicionar_Click(object sender, EventArgs e)
         {
             if (String.IsNullOrWhiteSpace(txtNome.Text) || !txtTel.MaskFull)
@@ -31,29 +63,20 @@ namespace ListaTelefonica
                 MessageBox.Show("Insira o nome e telefone");
                 return;
             }
-            lista[itens, 0]= txtNome.Text;
-            lista[itens, 1]= txtTel.Text;
-            itens++;
+            if (Length(lista) >= MAX)
+            {
+                MessageBox.Show("Lista cheia!!!!!!!!!!!!!!!");
+                return;
+            }
+            lista[Length(lista)] = new string[] { txtNome.Text, txtTel.Text };
             Atualizar();
 
         }
-        void Atualizar()
-        {
-            dgvLista.Rows.Clear();
-            for (int i = 0; i < itens; i++)
-            {
-                DataGridViewRow row = new DataGridViewRow();
-                row.CreateCells(dgvLista);
-                for (int j = 0; j < 2; j++)
-                {
-                    row.Cells[j].Value = lista[i, j];
-                }
-                dgvLista.Rows.Add(row);
-            }
-        }
+        
 
         private void btRemove_Click(object sender, EventArgs e)
         {
+            int itens = Length(lista);
             if (dgvLista.SelectedCells.Count == 0)
             {
                 MessageBox.Show("Selecione um item para remover");
@@ -62,13 +85,13 @@ namespace ListaTelefonica
 
             int indice = dgvLista.SelectedCells[0].RowIndex;
 
-            if (indice < 0 || indice >= itens)
+            if (indice < 0 || indice >= lista.Length)
             {
                 MessageBox.Show("Selecione um item válido para remover");
                 return;
             }
             DialogResult resposta = MessageBox.Show(
-                $"Deseja realmente remover {lista[indice, 0]}?",
+                $"Deseja realmente remover {lista[indice][0]}?",
                 "Confirmação",
                 MessageBoxButtons.YesNo);
 
@@ -76,13 +99,14 @@ namespace ListaTelefonica
             {
                 for (int i = indice; i < itens - 1; i++)
                 {
-                    lista[i, 0] = lista[i + 1, 0];
-                    lista[i, 1] = lista[i + 1, 1];
+                    lista[i][0] = lista[i + 1][0];
+                    lista[i][1] = lista[i + 1][1];
                 }
                 itens--;
-                lista[itens, 0] = null;
-                lista[itens, 1] = null;
+                lista[itens][0] = null;
+                lista[itens][1] = null;
                 Atualizar();
-                }
+            }
+        }
     }
 }
